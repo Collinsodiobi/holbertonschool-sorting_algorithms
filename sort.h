172a17d46fc52334f26635099eba3b4cf24614c5
Collinsodiobi <Collinsodi10@gmail.com>
2026-05-03 22:34:34 +0000

    Fix Betty documentation for struct listint_s

--- sort.h
+++ sort.h
@@ -3,7 +3,14 @@
 
 #include <stddef.h>
 
-/* Doubly linked list structure */
+/**
+ * struct listint_s - Doubly linked list node
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous node
+ * @next: Pointer to the next node
+ *
+ * Description: Node of a doubly linked list used for sorting algorithms.
+ */
 typedef struct listint_s
 {
 	const int n;
@@ -11,11 +18,27 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* Provided functions */
+/* Function prototypes */
+
+/**
+ * print_array - Prints an array of integers
+ * @array: The array to be printed
+ * @size: Number of elements in the array
+ */
 void print_array(const int *array, size_t size);
+
+/**
+ * print_list - Prints a doubly linked list of integers
+ * @list: The list to be printed
+ */
 void print_list(const listint_t *list);
 
-/* Your function */
+/**
+ * bubble_sort - Sorts an array of integers in ascending order
+ * using the Bubble sort algorithm
+ * @array: Array of integers
+ * @size: Number of elements in the array
+ */
 void bubble_sort(int *array, size_t size);
 
-#endif
+#endif /* SORT_H */
