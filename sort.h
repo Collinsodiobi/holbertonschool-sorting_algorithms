bdd89db74a9cf8cceb69eeb646d75e877f2e4c2f
Collinsodiobi <Collinsodi10@gmail.com>
2026-05-03 22:56:25 +0000

    Insertion sort implementation

--- sort.h
+++ sort.h
@@ -40,5 +40,6 @@ void print_list(const listint_t *list);
  * @size: Number of elements in the array
  */
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif /* SORT_H */
